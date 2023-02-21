e5cafb95b7b28bbfbde66b32ff29fa773b5da4d5
Sandhy <tiwarisandhyprakash@gmail.com>
2023-02-21 16:36:37 +0530

    first

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,17 +9,35 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        //Hint: Car extends Vehicle
+    public int getCurrentGear() {
+        return currentGear;
     }
 
-    public void changeGear(int newGear){
+    public void setCurrentGear(int currentGear) {
+        this.currentGear = currentGear;
+    }
 
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        //Hint: Car extends Vehicle
+        super(name);
+        this.wheels=wheels;
+        this.doors=doors;
+        this.gears=gears;
+        this.isManual=isManual;
+        this.type=type;
+        this.seats=seats;
+    }
+
+    public void changeGear(int newGear) {
+        if (newGear <= this.gears) {
+            this.currentGear = newGear;
+            System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+        }
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+           setCurrentSpeed(newSpeed);
+           setCurrentDirection(newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
